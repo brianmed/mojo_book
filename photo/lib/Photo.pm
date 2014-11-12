a3e16833f4a98c0dd38a800ef7d2789e71f9fca7
Brian Medley <pub-github@bmedley.org>
2014-11-12 05:20:11 -0600

    Some application edits

--- photo/lib/Photo.pm
+++ photo/lib/Photo.pm
@@ -1,15 +1,28 @@
 package Photo;
+
 use Mojo::Base 'Mojolicious';
 
 # This method will run once at server start
 sub startup {
   my $self = shift;
 
+  $self->log->level("debug");
+
+  my $site_config = $self->plugin("Config" => {file => '/opt/photo'});
+  $self->helper(site_dir => \&site_dir);
+  $self->helper(site_config => \&site_config);
+  $self->site_dir($$site_config{site_dir});
+  $self->site_config($site_config);
+
+  $self->plugin(AccessLog => {uname_helper => 'set_username', log => "$$site_config{site_dir}/log/access.log", format => '%h %l %u %t "%r" %>s %b %D "%{Referer}i" "%{User-Agent}i"'});
+
+  $self->secrets([$$site_config{site_secret}]);
+  
   # Router
   my $r = $self->routes;
 
   # Normal route to controller
-  $r->get('/')->to(controller => 'Index', action => 'slash');
+  $r->get('/')->to(controller => 'Index', action => 'slash'); # (*@\label{_appendix_route}@*)
 }
 
 1;
--- photo/lib/Photo/Controller/Index.pm
+++ photo/lib/Photo/Controller/Index.pm
@@ -2,10 +2,10 @@ package Photo::Controller::Index;
 
 use Mojo::Base 'Mojolicious::Controller';
 
-sub slash {
+sub slash { # (*@\label{_appendix_slash}@*)
     my $c = shift;
 
-    $c->reply->static('index.htm');
+    $c->reply->static('index.htm'); # (*@\label{_appendix_static}@*)
 }
 
 1;
