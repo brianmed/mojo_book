3fdffdabb935a56240fec19615e35f727fcbdc6c
Brian Medley <pub-github@bmedley.org>
2016-04-22 18:21:08 -0400

    Cleanup and session examples

--- ex2_4.pl
+++ ex2_4.pl
@@ -1,26 +1,23 @@
 use Mojolicious::Lite;
 
-get '/:name' => sub { # (*@\label{_ex2_4_placeholder}@*)
+get '/:name' => {name => ''} => sub { # (*@\label{_ex2_4_placeholder}@*)
     my $c = shift;
 
     my $name = $c->param("name");
 
-    $c->app->log->debug("get");
-    my $url = $self->url_for('/Example')->to_abs;
-
     $c->stash(name => $name); # (*@\label{_ex2_4_stash}@*)
-    $c->stash(url => $url);
 
     $c->render("slash");
 };
+
 app->start;
 
 __DATA__
 
 @@ slash.html.ep
 
-% if (stash('name')) {  <!-- (*@\label{_ex2_4_stash_usage}@*) -->
+% if (stash('name')) {  # <!-- (*@\label{_ex2_4_stash_usage}@*) -->
     You are <%= stash('name') %>
 % } else {
-    Please pass in a name to the url like so '<\%= stash('url') %>'.
+    Please pass in a name to the url like so '<%= url_for('/Ben')->to_abs %>'.
 % }
