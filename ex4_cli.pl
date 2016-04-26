721fefd2e9ab79a82bb1e61f6272c7e5d52c743a
Brian Medley <pub-github@bmedley.org>
2016-04-25 21:04:06 -0400

    Non-blocking for ex4_cli.pl

--- ex4_cli.pl
+++ ex4_cli.pl
@@ -7,23 +7,37 @@ use Mojo::UserAgent;
 my $ua = Mojo::UserAgent->new;
 my $addr = shift // die("Please pass in an IP");
 
-# Get latitude, longitude
-my $ip = $ua->get(sprintf("http://ip-api.com/json/%s", $addr));
+# Non-blocking requests (synchronized with a delay)
+Mojo::IOLoop->delay(
+  sub {
+    my $delay = shift;
 
-# Setup weather query
-my $query = sprintf(
-    "lat=%s&lon=%s&unit=0&lg=english&FcstType=json",
-    $ip->res->json->{lat},
-    $ip->res->json->{lon}
-);
-my $url = sprintf("http://forecast.weather.gov/MapClick.php?%s", $query);
-my $weather = $ua->get($url);
+    my $ip_query = sprintf("http://ip-api.com/json/%s", $addr);
+    $ua->get($ip_query => $delay->begin);
+  },
+  sub {
+    my ($delay, $ip) = @_;
 
-# Talk about the weather
-my $j = $weather->res->json;
-say(sprintf(
-    "$addr: %s: %s: %s", 
-    $j->{location}{areaDescription},
-    $j->{time}{startPeriodName}[0],
-    $j->{data}{text}[0]
-));
+    # Setup weather query
+    my $query = sprintf(
+        "lat=%s&lon=%s&unit=0&lg=english&FcstType=json",
+        $ip->res->json->{lat},
+        $ip->res->json->{lon}
+    );
+    my $url = sprintf("http://forecast.weather.gov/MapClick.php?%s", $query);
+
+    $ua->get($url => $delay->begin);
+  },
+  sub {
+    my ($delay, $weather) = @_;
+
+    # Talk about the weather
+    my $j = $weather->res->json;
+    say(sprintf(
+        "$addr: %s: %s: %s", 
+        $j->{location}{areaDescription},
+        $j->{time}{startPeriodName}[0],
+        $j->{data}{text}[0]
+    ));
+  }
+)->wait;
